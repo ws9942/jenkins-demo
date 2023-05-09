de69da5c4efbcd16c63a42a56293e6a701dc83e7
“u <mr_malone@163.com>
2023-05-09 09:58:37 +0800

    提交demo2

---
+++ demo2/src/main/java/com/example/demo/testController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class testController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+}
